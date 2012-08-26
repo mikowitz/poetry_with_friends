4246c9146c62fbf915f981f46a82e82c647e1ace
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-08-26 19:59:13 -0400

    Basic rails app w/Devise.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoetryWithFriends::Application.config.secret_token = 'd59d78ca9ff644117ebca56e176ff7a2f9e96658c0aacb1757480d561facf33da363e52920796de27593adc5433717119cf2644a96a83a9b271fdd3611058aff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PoetryWithFriends::Application.config.session_store :cookie_store, key: '_poetry_with_friends_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PoetryWithFriends::Application.config.session_store :active_record_store
