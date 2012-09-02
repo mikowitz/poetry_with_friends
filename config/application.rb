833becea70bd1733c8138eed6c3927c7d903dd28
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-09-01 21:45:04 -0400

    WCGW?

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module PoetryWithFriends
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( application.css.scss foundation.css foundation.js )
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
